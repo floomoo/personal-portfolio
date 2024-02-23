71423107945c21c17e72b90f8a03e4432e07db37
floomoo <elvinthomasusa@gmail.com>
2024-02-23 05:12:47 -0500

    PERSOL

---
+++ src/Components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className='w-[90px] h-[90px]
+        bg-black'>
+            <img src='/logo.png'
+            className=''  />
+    </div>
+    </div>
+  )
+}
+
+export default Header
