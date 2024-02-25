dcfb21eee18395fbf0dddd346d2b2fabc4d6f063
floomoo <elvinthomasusa@gmail.com>
2024-02-25 02:56:19 -0500

    icon profilepic

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header'
+import SideBar from './Components/SideBar'
+import ProfilePic from './Components/ProfilePic'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,16 @@ function App() {
   return (
     <>
     <Header/>
+    <SideBar/>
+      <div className='ml-[90px] grid grid-cols-1 md:grid-cols-3 '>
+        <div className='col-span-2'>
+          User Data
+        </div>
+        <div>
+          <ProfilePic/>
+        </div>
+      </div>
+
     </>
   )
 }
---
+++ src/Components/ProfilePic.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePic() {
+  return (
+    <div>
+      ProfilePic
+    </div>
+  )
+}
+
+export default ProfilePic
