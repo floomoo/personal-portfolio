c922c4956325e2b1cec63f511a592f2f39f28bca
floomoo <elvinthomasusa@gmail.com>
2024-03-08 20:45:30 -0500

    yes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Strings from '../share/Strings'
+import { BiSolidQuoteAltLeft,BiSolidQuoteAltRight } from 'react-icons/bi'
 
 function About() {
   return (
-    <div>
-    <h2>{Strings.ABOUT_HEADER}</h2> 
+    <div className='mt-12 px-44 text-center flex flex-col items-center'>
+        <h2 className='text-[40px] font-bold'>{Strings.ABOUT_HEADER}</h2> 
+        <BiSolidQuoteAltLeft className='bg-green-500 p-3 text-[44px] rounded-full mt-6 text-white'/>
+        <h2 className='my-5 text-gray-400 text-[14px]'>{Strings.ABOUT_DESC}</h2>
+    <BiSolidQuoteAltRight className='bg-green-500 p-3 text-[44px] rounded-full mt-6 text-white'/>
+
     </div>
   )
 }
--- src/share/Strings.tsx
+++ src/share/Strings.tsx
@@ -1,4 +1,4 @@
 export default{
-  ABOUT_HEADER:'My name is Elvin Thomas, \n  I am a very hard worker always try my best to be one hour early on any job for proper tim management',
-  ABOUT_DESC:"I am a passionate and dedicated UI/UX designer ",
+  ABOUT_HEADER:'My name is Elvin Thomas, \n  I am a very hard worker always try my best to be one hour early on any job for proper time management',
+  ABOUT_DESC:"I am a passionate and dedicated UI/UX designer hard working with the best thinking ever, working hard and solving all my serious problems "
 }
